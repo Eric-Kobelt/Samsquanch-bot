616296699fb3a2c3747f09e763e20e249538ef88
Eric Kobelt <EricK@marsreturnship.com>
2022-07-16 20:50:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) =>{
+    if (message.content == "Pizza"){
+        message.reply("The modern incarnation of pizza was not invented in Italy until around 1890.") 
+        message.reply("The first American Pizzeria opened in 1905.")  
+        message.reply("Therefor 'authentic' Italian Pizza only predated NY pizza by roughly 15 years, ")
+        message.reply("and isn't particularily better than the American verson by virtue of a longer culinary tradition")
+    }
+
+})
+
+client.login(process.env.TOKEN)
